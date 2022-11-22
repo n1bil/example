7e52e0d0a81154a46843a95c881b6988342b22c1
nabil <nabil.nadzor@gmail.com>
2022-11-22 19:10:23 +0100

    implement division function

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,6 +5,7 @@ public class Launcher {
         System.out.println(add(a, b));
         System.out.println(sub(a, b));
         System.out.println(mul(a, b));
+        System.out.println(div(a, b));
     }
 
     private static int add(int a, int b) {
@@ -17,5 +18,9 @@ public class Launcher {
 
     private static int mul(int a, int b) {
         return a * b;
+    }
+
+    private static int div(int a, int b) {
+        return a / b;
     }
 }
