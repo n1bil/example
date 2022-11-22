684e08b8213504aa9660597ef92ed808f07f829c
nabil <nabil.nadzor@gmail.com>
2022-11-22 19:08:23 +0100

    implement subtraction function

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,9 +3,14 @@ public class Launcher {
         int a = 5;
         int b = 4;
         System.out.println(add(a, b));
+        System.out.println(sub(a, b));
     }
 
     private static int add(int a, int b) {
         return a + b;
+    }
+
+    private static int sub(int a, int b) {
+        return a - b;
     }
 }
